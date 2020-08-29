92c24b9a6ad04e77a6020c9cf632678f765f768f
Ktosi33 <54289137+Ktosi33@users.noreply.github.com>
2020-08-29 16:39:48 +0200

    Tracks
    -Added GameObjects: Player, tracks
    -Added track controller

---
+++ Assets/Scripts/Tracks.cs
@@ -0,0 +1,19 @@
+﻿using JetBrains.Annotations;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tracks : MonoBehaviour
+{
+public GameObject[] tracks;
+
+
+ public  GameObject this[int index]
+    {
+        get
+        {
+            return  this.tracks[index];
+        }
+    }
+
+}
---
+++ Assets/Scripts/playerMovement.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerMovement : MonoBehaviour
+{
+    private Tracks tracks;
+   
+    void Start()
+    {
+        tracks = GameObject.FindGameObjectWithTag("Tracks").GetComponent<Tracks>();
+    }
+}
